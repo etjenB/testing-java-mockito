f7462ba000ee63420bb89dff26b886aecdfd7bb9
etjen <buljubasicetjen4@gmail.com>
2025-07-23 19:34:32 +0200

    owner controller tests, mockito argument capture and answers

---
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -0,0 +1,112 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import guru.springframework.sfgpetclinic.fauxspring.BindingResult;
+import guru.springframework.sfgpetclinic.fauxspring.Model;
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.services.OwnerService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class OwnerControllerTest {
+    private static final String OWNERS_CREATE_OR_UPDATE_OWNER_FORM = "owners/createOrUpdateOwnerForm";
+    private static final String REDIRECT_OWNERS_5 = "redirect:/owners/5";
+    @Mock(lenient = true)
+    OwnerService ownerService;
+    @InjectMocks
+    OwnerController controller;
+    @Mock
+    BindingResult bindingResult;
+    @Captor
+    ArgumentCaptor<String> stringArgumentCaptor;
+
+    @BeforeEach
+    void setUp() {
+        given(ownerService.findAllByLastNameLike(stringArgumentCaptor.capture())).willAnswer(invocationOnMock -> {
+           List<Owner> owners = new ArrayList<>();
+           String name = invocationOnMock.getArgument(0);
+           if (name.equals("%Buck%")) {
+               owners.add(new Owner(1L, "Joe", "Buck"));
+               return owners;
+           } else if (name.equals("%NNNNN%")) {
+               return owners;
+           } else if (name.equals("%M%")) {
+               owners.add(new Owner(1L, "Joe", "M"));
+               owners.add(new Owner(2L, "Franc", "Ma"));
+               return owners;
+           }
+           throw new RuntimeException("Invalid argument");
+        });
+    }
+
+    @Test
+    void processFindFormWildcardStringAnnotation() {
+        Owner owner = new Owner(1L, "Joe", "Buck");
+        String viewName = controller.processFindForm(owner, bindingResult, null);
+        assertThat(stringArgumentCaptor.getValue()).isEqualTo("%Buck%");
+        assertThat(viewName).isEqualTo("redirect:/owners/1");
+    }
+
+    @Test
+    void processFindFormWildcardStringAnnotationNotFound() {
+        Owner owner = new Owner(1L, "Joe", "NNNNN");
+        String viewName = controller.processFindForm(owner, bindingResult, null);
+        assertThat(stringArgumentCaptor.getValue()).isEqualTo("%NNNNN%");
+        assertThat(viewName).isEqualTo("owners/findOwners");
+    }
+
+    @Test
+    void processFindFormWildcardStringAnnotationFoundMultiple() {
+        Owner owner = new Owner(1L, "Joe", "M");
+        String viewName = controller.processFindForm(owner, bindingResult, Mockito.mock(Model.class));
+        assertThat(stringArgumentCaptor.getValue()).isEqualTo("%M%");
+        assertThat(viewName).isEqualTo("owners/ownersList");
+    }
+
+    @Test
+    void processFindFormWildcardString() {
+        Owner owner = new Owner(1L, "Joe", "Buck");
+        final ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        controller.processFindForm(owner, bindingResult, null);
+        verify(ownerService).findAllByLastNameLike(captor.capture());
+        assertThat(captor.getValue()).isEqualTo("%Buck%");
+    }
+
+    @Test
+    void processCreationFormHasErrors() {
+        //given
+        Owner owner = new Owner(1l, "Jim", "Bob");
+        given(bindingResult.hasErrors()).willReturn(true);
+
+        //when
+        String viewName = controller.processCreationForm(owner, bindingResult);
+
+        //then
+        assertThat(viewName).isEqualToIgnoringCase(OWNERS_CREATE_OR_UPDATE_OWNER_FORM);
+    }
+
+    @Test
+    void processCreationFormNoErrors() {
+        //given
+        Owner owner = new Owner(5l, "Jim", "Bob");
+        given(bindingResult.hasErrors()).willReturn(false);
+        given(ownerService.save(any())).willReturn(owner);
+
+        //when
+        String viewName = controller.processCreationForm(owner, bindingResult);
+
+        //then
+        assertThat(viewName).isEqualToIgnoringCase(REDIRECT_OWNERS_5);
+    }
+}
--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -9,6 +9,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.*;
@@ -22,31 +23,31 @@ class SpecialitySDJpaServiceTest {
 
     @Test
     void deleteById() {
-        specialitySDJpaService.deleteById(1l);
-        specialitySDJpaService.deleteById(1l);
-        verify(specialtyRepository, times(2)).deleteById(1l);
+        specialitySDJpaService.deleteById(1L);
+        specialitySDJpaService.deleteById(1L);
+        verify(specialtyRepository, times(2)).deleteById(1L);
     }
 
     @Test
     void deleteByIdAtLeastOnce() {
-        specialitySDJpaService.deleteById(1l);
-        specialitySDJpaService.deleteById(1l);
-        verify(specialtyRepository, atLeastOnce()).deleteById(1l);
+        specialitySDJpaService.deleteById(1L);
+        specialitySDJpaService.deleteById(1L);
+        verify(specialtyRepository, atLeastOnce()).deleteById(1L);
     }
 
     @Test
     void deleteByIdAtMost() {
-        specialitySDJpaService.deleteById(1l);
-        specialitySDJpaService.deleteById(1l);
-        verify(specialtyRepository, atMost(5)).deleteById(1l);
+        specialitySDJpaService.deleteById(1L);
+        specialitySDJpaService.deleteById(1L);
+        verify(specialtyRepository, atMost(5)).deleteById(1L);
     }
 
     @Test
     void deleteByIdNever() {
-        specialitySDJpaService.deleteById(1l);
-        specialitySDJpaService.deleteById(1l);
-        verify(specialtyRepository, atMost(5)).deleteById(1l);
-        verify(specialtyRepository, never()).deleteById(5l);
+        specialitySDJpaService.deleteById(1L);
+        specialitySDJpaService.deleteById(1L);
+        verify(specialtyRepository, atMost(5)).deleteById(1L);
+        verify(specialtyRepository, never()).deleteById(5L);
     }
 
     @Test
@@ -57,25 +58,25 @@ class SpecialitySDJpaServiceTest {
     @Test
     void testFindById() {
         Speciality speciality = new Speciality();
-        when(specialtyRepository.findById(1l)).thenReturn(Optional.of(speciality));
-        Speciality foundSpeciality = specialitySDJpaService.findById(1l);
+        when(specialtyRepository.findById(1L)).thenReturn(Optional.of(speciality));
+        Speciality foundSpeciality = specialitySDJpaService.findById(1L);
         assertThat(foundSpeciality).isNotNull();
-        verify(specialtyRepository).findById(1l);
+        verify(specialtyRepository).findById(1L);
     }
 
     @Test
     void testFindByIdBDD() {
         //given
         Speciality speciality = new Speciality();
-        given(specialtyRepository.findById(1l)).willReturn(Optional.of(speciality));
+        given(specialtyRepository.findById(1L)).willReturn(Optional.of(speciality));
 
         //when
-        Speciality foundSpeciality = specialitySDJpaService.findById(1l);
+        Speciality foundSpeciality = specialitySDJpaService.findById(1L);
 
         //then
         assertThat(foundSpeciality).isNotNull();
-        //then(specialtyRepository).should().findById(1l);
-        then(specialtyRepository).should(times(1)).findById(1l);
+        //then(specialtyRepository).should().findById(1L);
+        then(specialtyRepository).should(times(1)).findById(1L);
         then(specialtyRepository).shouldHaveNoMoreInteractions();
     }
 
@@ -84,5 +85,44 @@ class SpecialitySDJpaServiceTest {
         Speciality speciality = new Speciality();
         specialitySDJpaService.delete(speciality);
         verify(specialtyRepository).delete(any(Speciality.class));
+    }
+
+    @Test
+    void testDoThrow() {
+        doThrow(new RuntimeException("boom")).when(specialtyRepository).delete(any());
+        assertThrows(RuntimeException.class, () -> specialitySDJpaService.delete(new Speciality()));
+        verify(specialtyRepository).delete(any());
+    }
+
+    @Test
+    void testSaveLambda() {
+        final String MATCH_ME = "MATCH_ME";
+        Speciality speciality = new Speciality();
+        speciality.setDescription(MATCH_ME);
+
+        Speciality savedSpecialty = new Speciality();
+        savedSpecialty.setId(1L);
+        
+        given(specialtyRepository.save(argThat(argument -> argument.getDescription().equals(MATCH_ME)))).willReturn(savedSpecialty);
+        
+        Speciality returnedSpecialty = specialitySDJpaService.save(speciality);
+        
+        assertThat(returnedSpecialty.getId()).isEqualTo(1L);
+    }
+
+    @Test
+    void testSaveLambdaNoMatch() {
+        final String MATCH_ME = "MATCH_ME";
+        Speciality speciality = new Speciality();
+        speciality.setDescription("NOT_MATCH");
+
+        Speciality savedSpecialty = new Speciality();
+        savedSpecialty.setId(1L);
+
+        given(specialtyRepository.save(argThat(argument -> !argument.getDescription().equals(MATCH_ME)))).willReturn(savedSpecialty);
+
+        Speciality returnedSpecialty = specialitySDJpaService.save(speciality);
+
+        assertThat(returnedSpecialty.getId()).isEqualTo(1L);
     }
 }
